2159fd70cae407f87f40075853ee8c2840dced4c
W̷̛͒i̶̓͗l̷͗̌ľ̶͘ <62452284+Ugric@users.noreply.github.com>
2022-06-04 20:49:59 +0100

    fix type for post

--- pages/api/watchtime.ts
+++ pages/api/watchtime.ts
@@ -5,7 +5,8 @@ export default async (
   res: NextApiResponse
 ) => {
     if (req.method == "POST") {
-        const data:{id: string, time: number}[] =req.body
+        const data: { id: string, time: number }[] = req.body
+        
        return res.status(200).send(undefined);
     }
     return res.status(400).send(undefined);
