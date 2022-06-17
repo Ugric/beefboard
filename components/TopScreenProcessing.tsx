ca05920a4dadc8e49556717cb474a7083b8ea651
W̷̛͒i̶̓͗l̷͗̌ľ̶͘ <62452284+Ugric@users.noreply.github.com>
2022-06-17 13:03:09 +0100

    fix recaptcha

--- components/TopScreenProcessing.tsx
+++ components/TopScreenProcessing.tsx
@@ -4,7 +4,8 @@ const TopScreenProcessing = ({
     progress
 }: {
     progress: number
-}) => {
+    }) => {
+    
     return <div className={styles.progress} style={{
         width: `${(progress>0? progress<1?progress:1:0) * 100}%`
         , opacity: progress<1?1:0
